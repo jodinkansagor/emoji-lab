a631474326f837a618a61e4d9e62456fea58fbd7
Jodi N Kansagor <joedvice@gmail.com>
2020-02-12 16:19:08 -0800

    hyper test passed

--- src/selectors/moodSelector.test.js
+++ src/selectors/moodSelector.test.js
@@ -1,4 +1,4 @@
-import { isTired } from "./moodSelector";
+import { isTired, isHyper } from "./moodSelector";
 
 describe('mood selectors', () => {
   it('is tired when it is supposed to be tired', () => {
@@ -6,5 +6,12 @@ describe('mood selectors', () => {
     const tired = isTired(state);
 
     expect(tired).toEqual(true);
+  });
+
+  it('is hyper when it is supposed to be hyper', () => {
+    const state = { coffees: 6 };
+    const hyper = isHyper(state);
+
+    expect(hyper).toEqual(true);
   });
 });
